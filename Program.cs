73ae1972a3ea0f42de003b324838ded46e6f3fa5
Emre YİĞİT <emreygt67@icloud.com>
2021-03-20 14:50:12 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GörselProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class ÖğrenciBilgi
+    {
+
+    }
+    class DersSec
+    {
+        public int MyProperty { get; set; }
+    }
+}
